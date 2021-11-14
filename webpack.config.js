250f5fb15f23e5cbf47ee176e88cfeb8a3074104
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2021-11-14 18:22:53 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public/'),
+            publicPath: '/'
+        }
+    }
+}
